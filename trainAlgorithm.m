8c320e9b68cf9038340f6db6627e2b2312be13be
wskelly <wskelly@DMBQ2F3.olin.edu>
2021-11-03 22:32:21 -0400

    Now identifies song instead of train algorithm

--- trainAlgorithm.m
+++ trainAlgorithm.m
@@ -1,16 +1,17 @@
-% The Big'ole overarching function that does the whole shebang 
-% this doc comment is no longer even accurate because the scope of this
-% function has grown out of it's original intented purpose
+% Identifies a song as a member of a knwon genre or as a member of a new
+% genre
 %
-% PARAMS: trainData = the training dataset
-% Return: the algorithm vector X
+% PARAMS: testSong = the song to identify
+% Return: the index of the genre the song belongs to, or "new genre" if
+%         song does not belong to new genre
 
-function res = trainAlgorithm(trainData)
+function res = trainAlgorithm(testSong)
     %%%%%%%%%%%%%%%%%%
     % Create SongSpace
     %%%%%%%%%%%%%%%%%%
     
     % get principle eigensongs
+    trainData = %add training data
     V = getSongSpace(trainData)
     
     %Project training data onto V
@@ -29,10 +30,11 @@ function res = trainAlgorithm(trainData)
     % Etc. for each genre. Instead of numbers, genre names could be
     % incorperated into variable names
     
-    % Matrix of each average genre vector
+    % Matrices for each average genre vector and threshold
     numGenres      = %the number of genres that are "known", should be 
                         %equal to length(genreAveMatrix)
     genreAveMatrix = [G2Ave, G1Ave, %etc.] %NOTE: This is psudocode
+    thresholds     = [threshold1, threshold2, %etc] %NOTE: This is psudocode
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Identify Genre of unknown song
@@ -43,9 +45,22 @@ function res = trainAlgorithm(trainData)
     test_song = V' * testSongFreqs;
     
     % Calculate distance to closest genre
-    closestDist = intmax; %start as large as possible
+    closestDist  = intmax; %start as large as possible
+    closestIndex = -1 %set to -1 to throw error if not changed
     for index = 1:numGenres
         GenreAve = genreAveMatrix(index);
-        distToGenre = edist(
+        distToGenre = edist(test_song, GenreAve);
+        if distToGenre < closestDist
+            closestDist  = distToGenre;
+            closestIndex = index;
+        end
+    end
+    
+    % Check if member of genre and return
+    if closestDist < thresholds(closestIndex)
+        res = closestIndex %song has been recognized as part of genre 
+                           % represented by closestIndex
+    else
+        res = "new genre"; %song is not part of a known genre
     
 end
