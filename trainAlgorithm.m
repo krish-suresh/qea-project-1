350cafe5249bad7462a6cde4e2273ef8295688fa
wskelly <wskelly@DMBQ2F3.olin.edu>
2021-11-08 14:36:12 -0500

    Moved identification code to new function

--- trainAlgorithm.m
+++ trainAlgorithm.m
@@ -2,16 +2,19 @@
 % genre
 %
 % PARAMS: testSong = the song to identify
-% Return: the index of the genre the song belongs to, or "new genre" if
-%         song does not belong to new genre
+% Return: V              = the matrix whows columns are the eigensongs
+%         genreAveMatrix = a matrix whose columns are the average genre
+%             vectors for each song
+%         thresholds     = a vector containing the thresholds corresponding
+%             to the genres in genreAveMatrix
 
-function res = trainAlgorithm(testSong)
+function [V, genreAveMatrix, thresholds] = trainAlgorithm(testSong)
     %%%%%%%%%%%%%%%%%%
     % Create SongSpace
     %%%%%%%%%%%%%%%%%%
     
     % get principle eigensongs
-    trainData = %add training data
+    trainData = genre1Train, genre2Train %add training data
     V = getSongSpace(trainData)
     
     %Project training data onto V
@@ -35,32 +38,4 @@ function res = trainAlgorithm(testSong)
                         %equal to length(genreAveMatrix)
     genreAveMatrix = [G2Ave, G1Ave, %etc.] %NOTE: This is psudocode
     thresholds     = [threshold1, threshold2, %etc] %NOTE: This is psudocode
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Identify Genre of unknown song
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    % Get test song
-    testSongFreqs = songToVector(testSong);
-    test_song = V' * testSongFreqs;
-    
-    % Calculate distance to closest genre
-    closestDist  = intmax; %start as large as possible
-    closestIndex = -1 %set to -1 to throw error if not changed
-    for index = 1:numGenres
-        GenreAve = genreAveMatrix(index);
-        distToGenre = edist(test_song, GenreAve);
-        if distToGenre < closestDist
-            closestDist  = distToGenre;
-            closestIndex = index;
-        end
-    end
-    
-    % Check if member of genre and return
-    if closestDist < thresholds(closestIndex)
-        res = closestIndex %song has been recognized as part of genre 
-                           % represented by closestIndex
-    else
-        res = "new genre"; %song is not part of a known genre
-    
 end
